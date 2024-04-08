8928c703621a18fdcd9f12627d5a985579c96916
Rayyan Masood <142214744+RayyanMasood@users.noreply.github.com>
2024-04-09 00:22:59 +0500

    Solved problems

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(gArr, ...others) {
+    let nArr = []
+    // gArr = hArr;
+    // indexOfVal = hArr.indexOf(vToRem)
 
+    for (let val of others){ 
+        for (i = 0; i < gArr.length; i++) {
+            if (val === gArr[i]) {
+                gArr.splice(i, 1)
+                i--
+            }
+        }
+    }
+
+    console.log(gArr)
+    return gArr
 };
+
+removeFromArray([1,2,2,4], 2)
 
 // Do not edit below this line
 module.exports = removeFromArray;
