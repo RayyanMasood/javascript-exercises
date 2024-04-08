63322ae718ffdee6021d548560eb4f5c250a02ff
Rayyan Masood <142214744+RayyanMasood@users.noreply.github.com>
2024-04-08 22:33:33 +0500

    Solved problems

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(strGiven, nOfTImes) {
+    if (nOfTImes < 0) {
+        return 'ERROR'
+    }
 
+    if (strGiven === '') {
+        return ''
+    }
+    
+    let concatString = '';
+    for (i = 0; i<nOfTImes; i++) {
+        concatString = concatString + strGiven;
+    }
+    return concatString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(gString) {
+    let concatString = '';
 
+    if (gString === '') {
+        return ''
+    } else if (gString.length === 1) {
+        return gString;
+    }
+
+    for (i = gString.length - 1; i >= 0; i--) {
+        concatString = concatString + gString[i];
+    }
+    // print(concatString)
+    return concatString
 };
 
 // Do not edit below this line
