4a5f6f70435cadac160ec0ad71f1204505011753
Rayyan Masood <142214744+RayyanMasood@users.noreply.github.com>
2024-04-09 18:20:56 +0500

    Solved problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let val of arr) {
+    sum += val;
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let comp  = 1;
+  for (let val of arr) {
+    comp *= val;
+  }
 
+  return comp;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let compute = 1;
+	for (i = 1; i <= num; i++) {
+    compute *= i;
+  }
+
+  return compute
 };
 
 // Do not edit below this line
